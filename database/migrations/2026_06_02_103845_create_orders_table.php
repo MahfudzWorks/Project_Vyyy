3a3286e1762f5ee0d01125195e11623511d04290
Mahfudz Alfanani Syaviqi <mahfudzworksss@gmail.com>
2026-06-02 03:45:05 -0700

    commit orders admin db

--- database/migrations/2026_06_02_103845_create_orders_table.php
+++ database/migrations/2026_06_02_103845_create_orders_table.php
@@ -12,7 +12,38 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
+
             $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('judul');
+
+            $table->text('deskripsi');
+
+            $table->date('deadline');
+
+            $table->decimal('harga', 12, 2)
+                ->nullable();
+
+            $table->enum('status', [
+                'pending',
+                'konsultasi',
+                'menunggu_pembayaran',
+                'diproses',
+                'revisi',
+                'selesai',
+                'dibatalkan'
+            ])->default('pending');
+
+            $table->string('file_tugas')
+                ->nullable();
+
+            $table->string('hasil_tugas')
+                ->nullable();
+
             $table->timestamps();
         });
     }
